14dcbf54c0910c279e1fc1f418dc3d4bf6e91405
PrometheusDevvv <prometheus.devv@gmail.com>
2024-09-12 04:19:06 -0700

    Applied styles and display cards in app.jsx for testing purposes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Card from "./components/Card"
+import CardLayout from "./components/CardLayout"
 import FiltersContainer from "./components/searchBar/FiltersContainer"
 import Layout from "./Layouts/Layout"
 
@@ -7,6 +9,74 @@ function App() {
      <FiltersContainer>
 
      </FiltersContainer>
+     
+        
+          <CardLayout>
+          <Card
+                backgroundImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfMny3wIRYrYLdGXaDJyleSTjZ1j8PuzWuGA&s"
+                price="80 000 ₾"
+                location="თბილისი, ი. ჭავჭავაძის 53"
+                bedCount={2}
+                areaValue={{ value: '55 მ', subvalue: '2' }}
+                additionalInfo="0160"
+                tagText="ქირავდება"
+            />
+             <Card
+                backgroundImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfMny3wIRYrYLdGXaDJyleSTjZ1j8PuzWuGA&s"
+                price="80 000 ₾"
+                location="თბილისი, ი. ჭავჭავაძის 53"
+                bedCount={2}
+                areaValue={{ value: '55 მ', subvalue: '2' }}
+                additionalInfo="0160"
+                tagText="ქირავდება"
+            />
+             <Card
+                backgroundImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfMny3wIRYrYLdGXaDJyleSTjZ1j8PuzWuGA&s"
+                price="80 000 ₾"
+                location="თბილისი, ი. ჭავჭავაძის 53"
+                bedCount={2}
+                areaValue={{ value: '55 მ', subvalue: '2' }}
+                additionalInfo="0160"
+                tagText="ქირავდება"
+            />
+             <Card
+                backgroundImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfMny3wIRYrYLdGXaDJyleSTjZ1j8PuzWuGA&s"
+                price="80 000 ₾"
+                location="თბილისი, ი. ჭავჭავაძის 53"
+                bedCount={2}
+                areaValue={{ value: '55 მ', subvalue: '2' }}
+                additionalInfo="0160"
+                tagText="ქირავდება"
+            />
+             <Card
+                backgroundImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfMny3wIRYrYLdGXaDJyleSTjZ1j8PuzWuGA&s"
+                price="80 000 ₾"
+                location="თბილისი, ი. ჭავჭავაძის 53"
+                bedCount={2}
+                areaValue={{ value: '55 მ', subvalue: '2' }}
+                additionalInfo="0160"
+                tagText="ქირავდება"
+            />
+             <Card
+                backgroundImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfMny3wIRYrYLdGXaDJyleSTjZ1j8PuzWuGA&s"
+                price="80 000 ₾"
+                location="თბილისი, ი. ჭავჭავაძის 53"
+                bedCount={2}
+                areaValue={{ value: '55 მ', subvalue: '2' }}
+                additionalInfo="0160"
+                tagText="ქირავდება"
+            />
+             <Card
+                backgroundImage="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Strawberries.jpg/1200px-Strawberries.jpg"
+                price="80 000 ₾"
+                location="თბილისი, ი. ჭავჭავაძის 53"
+                bedCount={2}
+                areaValue={{ value: '55 მ', subvalue: '2' }}
+                additionalInfo="0160"
+                tagText="ქირავდება"
+            />
+          </CardLayout>
+
     </Layout>
   )
 }
